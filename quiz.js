ea1a0ffd53a1155702ea928e3cca93e6bbf86ffb
tony913 <tonycrosby96@gmail.com>
2020-10-06 14:36:20 -0500

    updating

--- quiz.js
+++ quiz.js
@@ -1,32 +1,32 @@
-//arrray of the quiz questions, avaialble choices, and correct answers     
+//question, choices, answers     
 var questions = [{
-    title: "Which of the following function of an array object adds one or more elements to the front of an array and returns the new length of the array?",
-    choices: ["unshift( )", "sort( )", "splice( )", "toString( )"],
-    answer: "unshift( )"
+    "title": "Which of the following is the correct syntax to display 'Bootcamp' in an alert box using JavaScript?",
+    "choices": ["alertbox('Bootcamp');","msg('Bootcamp');", "msgalert('Bootcamp');", "alert('Bootcamp');"],
+    "answer": "alert('Bootcamp');"
 },
 {
-    title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
-    choices: ["last( )", "put( )", "push( )", "pop( )"],
-    answer: "push( )"
+    "title": "Which of the following is not a reserved word in JavaScript?",
+    "choices": ["interface", "program", "throws", "short"],
+    "answer": "program"
 },
 {
-    title: " Which built-in method returns the characters in a string beginning at the specified location?",
-    choices: ["substr( )", "getSubstring( )", "slice( )", "None of the above."],
-    answer: "substr( )"
+    "title": " How do you create a function in JavaScript?",
+    "choices": ["function:myFunction()", "function = myFunction()", "function myFunction()"],
+    "answer": "function myFunction()"
 },
 {
-    title: "Which of the following function of an array object adds and/or removes elements from an array?",
-    choices: ["toSource( )", "sort( )", "unshift( )", "splice( )"],
-    answer: "splice( )"
+    "title": "The external JavaScript file must contain the script tag?",
+    "choices": [" true ", " false "],
+    "answer": " false "
 },
 {
-    title: "Which of the following function of String object combines the text of two strings and returns a new string?",
-    choices: ["add( )", "concat( )", " merge( )", "append( )"],
-    answer: "concat( )"
+    "title": "Where is the correct place to insert a JavaScript?",
+    "choices": ["the body section", "both the head section and the body section are correct.", "the head section"],
+    "answer": "both the head section and the body section are correct."
 }
 ]
 
-//setting the numerical variables for the functions.. scores and timers.. 
+//setting scores and timers.. 
 var score = 0;
 var currentQuestion = -1;
 var timeLeft = 0;
@@ -41,7 +41,7 @@ document.getElementById("timeLeft").innerHTML = timeLeft;
 timer = setInterval(function() {
     timeLeft--;
     document.getElementById("timeLeft").innerHTML = timeLeft;
-    //proceed to end the game function when timer is below 0 at any time
+    //end the game when timer is below 0 at any time
     if (timeLeft <= 0) {
         clearInterval(timer);
         endGame(); 
@@ -51,41 +51,41 @@ timer = setInterval(function() {
 next();
 }
 
-//stop the timer to end the game 
+//stop the timer to end game 
 function endGame() {
 clearInterval(timer);
 
 var quizContent = `
 <h2>Game over!</h2>
-<h3>You got a ` + score +  ` /100!</h3>
-<h3>That means you got ` + score / 20 +  ` questions correct!</h3>
-<input type="text" id="name" placeholder="Initials"> 
+<p>You got a ` + score +  ` /100!</p>
+<p>you got ` + score / 20 +  ` questions correct!</p>
+<input type="text" id="name" placeholder="Initials here"> 
 <button onclick="setScore()">Set score!</button>`;
 
 document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-//store the scores on local storage
+//store the score in local storage
 function setScore() {
 localStorage.setItem("highscore", score);
 localStorage.setItem("highscoreName",  document.getElementById('name').value);
 getScore();
 }
 
-
+// get the score depending how many questions were correct
 function getScore() {
 var quizContent = `
 <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
-<h1>` + localStorage.getItem("highscore") + `</h1><br> 
+<h1>` + localStorage.getItem("highscore") + `</h1><br>
 
-<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+<button onclick="clearScore()">Clear score!</button></br><button onclick="resetGame()">Play Again!</button>
 
 `;
 
 document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-//clears the score name and value in the local storage if the user selects 'clear score'
+//clears the score name and value if the user selects 'clear score'
 function clearScore() {
 localStorage.setItem("highscore", "");
 localStorage.setItem("highscoreName",  "");
@@ -93,7 +93,7 @@ localStorage.setItem("highscoreName",  "");
 resetGame();
 }
 
-//reset the game 
+//resets game 
 function resetGame() {
 clearInterval(timer);
 score = 0;
@@ -115,7 +115,7 @@ var quizContent = `
 document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-//deduct 15seconds from the timer if user chooses an incorrect answer
+//minus 15seconds from timer if user chooses an incorrect answer
 function incorrect() {
 timeLeft -= 15; 
 next();
@@ -127,7 +127,7 @@ score += 20;
 next();
 }
 
-//loops through the questions 
+//loops through questions 
 function next() {
 currentQuestion++;
 
@@ -139,12 +139,12 @@ if (currentQuestion > questions.length - 1) {
 var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
 
 for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
-    var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
-    buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+    var buttonCode = "<button onclick=\"[answer]\">[choices]</button></br>"; 
+    buttonCode = buttonCode.replace("[choices]", questions[currentQuestion].choices[buttonLoop]);
     if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
-        buttonCode = buttonCode.replace("[ANS]", "correct()");
+        buttonCode = buttonCode.replace("[answer]", "correct()");
     } else {
-        buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        buttonCode = buttonCode.replace("[answer]", "incorrect()");
     }
     quizContent += buttonCode
 }
